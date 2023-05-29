b2d94452492601200228703ae497d03ccb0a5250
marzieh kamali <mayhey1@gmail.com>
2023-05-29 16:14:11 +0000

    updates

---
+++ assertArraysEqual.js
@@ -0,0 +1,14 @@
+function eqArrays(arr1, arr2) {                 // can use arr1 or a
+  if (arr1.length !== arr2.length) {            // start with testing length, if length is not equal, your array is false from the beginning
+    console.log(`🛑🛑🛑Assertion Failed: ${arr1} !== arr2`);
+  }
+
+  for (let i = 0; i < arr1.length; i++) {       // testing out each index to ensure its equal
+    if (arr1[i] !== arr2[i]) {
+      return console.log(`🛑🛑🛑Assertion Failed: ${arr1} !== arr2`);;
+    }
+  }
+  console.log(`✅✅✅Assertion Passed: ${arr1} === arr2`)
+}
+
+eqArrays([1, 2, 3], [1, 2, 9]);
