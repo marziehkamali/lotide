67f721684b449bbf1ab26041f5a2c54c01ab42e2
marziehkamali <mayhey1@gmail.com>
2023-08-26 17:54:19 -0600

    removed comments that were not needed

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,10 +1,5 @@
-
-/* let result = eqArrays([1, 2, 3], [1, 2, 3]);  // giving eqArrays a variable inorder to receive the result
-assertEqual(result, true); // => should PASS  // testing your result of true against expected of true.
-/*/
-
-const assertArraysEqual = (arr1, arr2) => {                 // can use arr1 or a
-  if (arr1.length !== arr2.length) {            // start with testing length, if length is not equal, your array is false from the beginning
+const assertArraysEqual = (arr1, arr2) => {                 
+  if (arr1.length !== arr2.length) {            
     console.log(`🛑🛑🛑Assertion Failed: ${arr1} !== ${arr2}`);
   }
 
