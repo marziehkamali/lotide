6189b49d098c4a6fdc52e3dd473cb7cb7ccbd061
marziehkamali <mayhey1@gmail.com>
2023-08-26 23:12:42 -0600

    Fixed bug logic to return result was wrong. Added assertArrayEqual test.

--- takeUntil.js
+++ takeUntil.js
@@ -1,23 +1,42 @@
-const assertArraysEqual = require("./assertArraysEqual")
+function eqArrays(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+const assertArraysEqual = function (arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`✅✅✅Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+      console.log(`🛑🛑🛑Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+};
 
 const takeUntil = function (array, callback) {
   const result = [];
   for (let i = 0; i < array.length; i++) {
     if (!callback(array[i])) {
       result.push(array[i]);
-    } else {
-      return "break";
+    } else{
+      return result;
     }
   }
-  return results1;
+  return result;
 };
 
 const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
 const results1 = takeUntil(data1, x => x < 0);
-assertArraysEqual(results1,[])
+assertArraysEqual(results1, [1, 2, 5, 7, 2]);
 
 const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
 const results2 = takeUntil(data2, x => x === ',');
-assertArraysEqual(results2,[])
+assertArraysEqual(results2, ["I've", "been", "to", "Hollywood"]);
 
 module.exports = takeUntil;
