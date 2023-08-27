42a6cc99d8312fdba6343f0cc0f95cb42716e38e
marziehkamali <mayhey1@gmail.com>
2023-08-27 00:07:29 -0600

    as per mentor feedback - renamed parameters to be generic

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,26 +1,25 @@
 const assertEqual = require("./assertEqual");
 
+const findKeyByValue = function (object, value) {
+
+  const keys = Object.keys(object);
+
+  for (let i = 0; i < keys.length; i++) {
+
+    const key = keys[i];
+    const valueAtKey = object[key];
+    if (valueAtKey === value) {
+      return key;
+    }
+  }
+  return undefined;
+}
+
 const bestTVShowsByGenre = {
   sci_fi: "The Expanse",
   comedy: "Brooklyn Nine-Nine",
   drama: "The Wire"
 };
-
-
-const findKeyByValue = function (object, value) {
-
-  const genresArr = Object.keys(bestTVShowsByGenre);
-
-  for (let i = 0; i < genresArr.length; i++) {
-
-    const genre = genresArr[i];
-    movieTitle = object[genre];
-    if (movieTitle === value) {
-      return genre;
-    }
-  }
-  return undefined;
-}
 
 assertEqual(findKeyByValue(bestTVShowsByGenre, "The Expanse"), "sci_fi");
 assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
